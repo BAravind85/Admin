225e9112718be647c396de4f26350f572c085fd1
BAravind85 <102968059+BAravind85@users.noreply.github.com>
2023-01-20 23:56:52 +0530

    login pages  directing

--- server/src/routes/route.js
+++ server/src/routes/route.js
@@ -1,11 +1,24 @@
 const express=require('express');
-const router=express.Router();
+const router=express();
 const adminController=require('../controller/adminController')
+// const companyController= require('../controller/companiesController')
 // const {authentication} = require('../middleware/authentication')
 
-router.post('/admin', adminController.createAdmin);
-router.post('/login', adminController.adminLogin);
+//admin registartion and login
+router.set('view engine','ejs');
+router.set('views','./views/admin');
 
+//* register
+router.get('/register', adminController.createAdmin);
+router.post('/register',adminController.insertAdmin)
+
+//*login
+router.get('/',adminController.loginLoad)
+router.get('/login',adminController.loginLoad)
+router.post('/login',adminController.verifylogin)
+
+router.get('/company',adminController.company)
 
 
 module.exports = router;
+//*http://127.0.0.1:3000/login
